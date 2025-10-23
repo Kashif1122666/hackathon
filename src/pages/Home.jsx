c8101400b2cfae34272a33b3db62aa404c44ab3a
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2025-10-23 12:54:38 +0500

    Initial commit - setup with Shadcn UI

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Button} from '../components/ui/button.tsx'
+import {Input} from '../components/ui/input.tsx'
+
+
+
+export default function Home() {
+  return (
+<>
+    <div>Home</div>
+    <Button>Click Me</Button>
+    <Input placeholder="Type here..." />
+    
+</>
+  )
+}
