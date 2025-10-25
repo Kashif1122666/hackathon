9d61dd9b22ed1c34dbf582e191c0106c262b4a50
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2025-10-26 01:30:08 +0500

    add dark theme by default..

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -2,9 +2,7 @@ import { useEffect, useState } from "react";
 
 export default function useTheme() {
   const [theme, setTheme] = useState(() => {
-    if (typeof window !== "undefined") {
-      return localStorage.getItem("theme") || "dark";
-    }
+    
     return "dark";
   });
 
